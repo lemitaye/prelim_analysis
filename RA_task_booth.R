540870c18c9327c503a9060d5965727e5caa01d8
Lemi Daba <tayelemi@gmail.com>
2022-04-13 15:27:23 +0100

    Final draft

--- RA_task_booth.R
+++ RA_task_booth.R
@@ -1,14 +1,20 @@
 
 
+# Preliminaries:
+rm(list = ls())
 library(tidyverse)
-library(matrixStats)  # needed to calculate weighted median
+library(matrixStats) # needed to calculate weighted median
 library(scales)
 library(ggpubr)
 theme_set(theme_light())
 
+# Load data:
 data <- read_csv("RA_21_22.csv")
 
+# Inspect data:
 glimpse(data)
+
+
 
 # 1. Please summarize key trends in median total wealth over the last 30 years
 # by race and education using plots and in writing.
@@ -16,7 +22,7 @@ glimpse(data)
 
 # Function to summarize by groups:
 summ_by <- function(var1, var2 = NULL) {
-  require(matrixStats) 
+  require(matrixStats)
 
   tbl <- data %>%
     mutate(wealth_total = asset_total - debt_total) %>%
@@ -26,18 +32,17 @@ summ_by <- function(var1, var2 = NULL) {
   return(tbl)
 }
 
+# Use function to summarize data:
 by_race <- summ_by(race)
 by_educ <- summ_by(education)
 by_race_educ <- summ_by(race, education)
 
-plot_by <- function(
-  tbl, y = median_wealth, by = race,
-  brks = c("white", "other", "Hispanic", "black"),
-  labs = c("Whites", "Other", "Hispanic", "Black")
-  ) {
-  
-  p <- tbl %>% 
-    ggplot(aes(year, {{y}}, color = {{by}}, linetype = {{by}})) +
+# Function to produce time series plots:
+plot_by <- function(tbl, y = median_wealth, by = race,
+                    brks = c("white", "other", "Hispanic", "black"),
+                    labs = c("Whites", "Other", "Hispanic", "Black")) {
+  p <- tbl %>%
+    ggplot(aes(year, {{ y }}, color = {{ by }}, linetype = {{ by }})) +
     geom_line(size = 0.85) +
     scale_y_continuous(labels = label_number_si()) +
     scale_color_discrete(breaks = brks, labels = labs) +
@@ -47,25 +52,22 @@ plot_by <- function(
       breaks = dplyr::count(data, year) %>% pull(year) %>% .[c(2, 6, 10)]
     ) +
     theme(legend.position = "top") +
-    labs(
-      x = "Year",
-      y = "Median Wealth (2016 dollars)",
-      color = "", linetype = ""
-    )
-    
+    labs(x = "Year", y = "Median Wealth (2016 dollars)", 
+         color = "", linetype = "")
+
   return(p)
 }
 
 # Figure 1
-plot_by(by_race)  
+plot_by(by_race)
 
 # Figure 2:
 plot_by(
-  by_educ, 
-  by = education, 
+  by_educ,
+  by = education,
   brks = c("college degree", "some college", "no college"),
   labs = c("College Degree", "Some College", "No College")
-  )
+)
 
 # Figure 3:
 plot_by(by_race_educ) +
@@ -77,30 +79,26 @@ plot_by(by_race_educ) +
     )
   ))
 
-
-
-# We have the expected story: whites and the educated are rich, blacks and the
-# uneducated are poor.
-
-# Figure 4
+# Figure 4:
 data %>%
   filter(race %in% c("white", "black")) %>%
   mutate(wealth_housing = asset_housing - debt_housing) %>%
   group_by(year, race) %>%
   summarise(median_housing = weightedMedian(wealth_housing, weight)) %>%
   plot_by(
-    by = race, 
-    y = median_housing, 
-    brks = c("white", "black"), 
+    by = race,
+    y = median_housing,
+    brks = c("white", "black"),
     labs = c("Whites", "Blacks")
-    )
-  
+  )
+
 
 # 3. Many households are not homeowners and so your analysis for the prior question includes many zeros
 # for housing wealth. Let’s dig deeper by focusing just on homeowners age 25 or older. Subsetting to
 # homeowners age 25 or older, please summarize trends in median housing and non-housing wealth for
 # black and white households. Which group had the largest loss in housing wealth, where 2007 is defined
 # as the base period? Please answer this question both in dollar terms and in proportional terms.
+
 
 # Calculate Median Wealth:
 owners_above_25 <- data %>%
@@ -122,15 +120,16 @@ owners_above_25 <- data %>%
   ) %>%
   mutate(wealth = str_remove(wealth, "median_"))
 
-# Plot (Figure )
+# Figure 5:
 owners_above_25 %>%
   ggplot(aes(factor(year), median, fill = race)) +
   geom_col(position = "dodge") +
-  facet_wrap(~wealth,
-             nrow = 2,
-             labeller = as_labeller(
-               c("housing" = "Housing", "non_housing" = "Non-Housing")
-             )
+  facet_wrap(
+    ~wealth,
+    nrow = 2,
+    labeller = as_labeller(
+      c("housing" = "Housing", "non_housing" = "Non-Housing")
+    )
   ) +
   scale_y_continuous(labels = label_number_si()) +
   scale_fill_discrete(
@@ -140,12 +139,11 @@ owners_above_25 %>%
   labs(
     x = "Year",
     y = "Median Wealth (2016 dollars)",
-    fill = "" # ,
-    # title = "Median Wealth by Type (Housing vs. Non-Housing) and Race"
+    fill = "" 
   )
 
 
-# Absolute and Proportional change:
+# Compute absolute and proportional change:
 change_2007 <- owners_above_25 %>%
   filter(year >= 2007, wealth == "housing") %>%
   pivot_wider(names_from = year, values_from = "median") %>%
@@ -176,9 +174,7 @@ change_abs <- change_2007 %>%
   ) +
   theme(legend.position = "top") +
   labs(
-    x = "Year",
-    y = "Median Housing Wealth\n (2016 dollars)",
-    fill = "" 
+    x = "Year", y = "Median Housing Wealth\n (2016 dollars)", fill = ""
   )
 
 # Plot percentage change:
@@ -189,37 +185,13 @@ change_pct <- change_2007 %>%
   geom_hline(yintercept = 0) +
   expand_limits(y = c(-0.25, 0.05)) +
   scale_y_continuous(labels = percent) +
-  scale_fill_discrete(
-    breaks = c("white", "black"),
-    labels = c("Whites", "Blacks")
-  ) +
+  scale_fill_discrete(breaks = c("white", "black"), 
+                      labels = c("Whites", "Blacks")) +
   theme(legend.position = "top") +
-  labs(
-    x = "Year",
-    y = "Percent",
-    fill = ""
-  )
+  labs(x = "Year", y = "Percent", fill = "")
 
-# Gather the two plots in the same figure:
+# Gather the two plots in the same figure to get Figure 6:
 ggarrange(
-  change_abs, change_pct, nrow = 1, common.legend = TRUE, legend = "top"
+  change_abs, change_pct,
+  nrow = 1, common.legend = TRUE, legend = "top"
 )
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
